cea49f02a6c231dba4b2982e57d87902dc1e0333
roman <roman951t@gmail.com>
2018-02-28 15:11:02 +0200

    fix layout

--- client/src/reduxBase/actions/index.js
+++ client/src/reduxBase/actions/index.js
@@ -90,16 +90,16 @@ export function userRegister(user) {
   };
 }
 
-export function userAuthAttempt(user) {
-  return {
-    type: actionTypes.USER_AUTH_ATTEMPT,
-    user,
-  };
-}
+// export function userAuthAttempt(user) {
+//   return {
+//     type: actionTypes.USER_AUTH_ATTEMPT,
+//     user,
+//   };
+// }
 
-export function userAuthorize(userId) {
-  return {
-    type: actionTypes.USER_AUTHORIZE,
-    userId,
-  };
-}
+// export function userAuthorize(userId) {
+//   return {
+//     type: actionTypes.USER_AUTHORIZE,
+//     userId,
+//   };
+// }
